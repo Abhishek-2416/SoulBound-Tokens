c43ece8158b535f0d3b624af10e691f16c2ef563
Abhishek Alimchandani <abhishekalimchandani1624@protonmail.com>
2023-10-15 16:19:42 +0530

    Tested SoulBound Token

--- test/SoulBoundTokenTest.t.sol
+++ test/SoulBoundTokenTest.t.sol
@@ -11,6 +11,31 @@ contract TestSoulBoundToken is Test{
         token = new CollegeDAO();
     }
 
+    function testIfTokenIdIncerementInSafeMint() public{
+        token.safeMint(address(0x1),"nft");
+        assertEq(token.getTokenCount(),1);
+
+        token.safeMint(address(0x2),"nft2");
+        assertEq(token.getTokenCount(),2);
+
+        // vm.expectRevert();
+        assert(token.getTokenCount() != 3);
+    }
+
+    function testSafeMintInSafeMint() public{
+        token.safeMint(address(0x1),"nft");
+        assertEq(token.ownerOf(0),address(0x1));
+        assertEq(token.getTokenCount(),1);
+    }
+
+    function testSetTokenURIInSafeMint() public{
+        token.safeMint(address(0x1),"nft");
+        assertEq(token.ownerOf(0),address(0x1));
+        assertEq(token.getTokenCount(),1);
+        assertEq(token.tokenURI(0) ,"nft");
+    }
+
+
     function testburnCanBeCalledOnlyByOwner() public{
         token.safeMint(address(0x1),"nft");
         vm.prank(address(0x1));
@@ -22,7 +47,13 @@ contract TestSoulBoundToken is Test{
         assert(token.getTokenCount() == 1);
         vm.prank(address(0x1));
         token.burn(0);
-        assert(token.ownerOf(token.getTokenCount()) == address(0));
+        assert(token.balanceOf(address(0x1)) == 0);
     }
-    
+
+    function testRevoke() public {
+        token.safeMint(address(0x1),"nft");
+        vm.prank(address(this));
+        token.revoke(0);
+        assert(token.balanceOf(address(0x1)) == 0);
+    }
 }
